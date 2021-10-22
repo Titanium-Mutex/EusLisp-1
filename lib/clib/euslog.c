eaf1d135e4180117dd615e591fbbb1afd5f3041b
Titanium-Mutex <matsui.toshihiro3@gmail.com>
2021-10-22 16:03:08 +0900

    euslog.c defines syslog function that uses syslog (3) make-cookie recognizes timezone for setting :expires parameter pgtable generates html-input-table depending on the sizes of fields. ez masks msgq key with 0xffff and ors with 0x500000 and 0x600000.

---
+++ lib/clib/euslog.c
@@ -0,0 +1,50 @@
+/*************************************************************
+/* euslog.c 2021-10-12 by Toshihiro Matsui
+/* % cc -c -fpic -DLinux -Dx86_64 -falign-functions=8 -DGCC -shared euslog.c
+/* % ld -o euslog.so -shared euslog.o
+/* From euslisp,
+/* (openlog "eventtag" option default-facility)
+/* (syslog priority "message")
+/***************************************/  
+
+#include "eus.h"
+#include <syslog.h>
+
+/*
+       void openlog(const char *ident, int option, int facility);
+       void syslog(int priority, const char *format, ...);
+       void closelog(void);
+*/
+
+pointer OPENLOG(context *ctx, int n, pointer argv[])
+{ char *identp; 
+  int option=0, facility=0;
+
+  ckarg2(1,3); /* ident, option, facility */
+  identp=argv[0]->c.str.chars;
+  fprintf(stderr, "OPENLOG!!! n=%d %s\n", n, identp);
+  if (n>=2) option=ckintval(argv[1]);
+  if (n==3) facility=ckintval(argv[2]);
+  openlog(identp, option, facility);
+  return(NIL);}
+
+pointer SYSLOG(context *ctx, int n, pointer argv[])
+{ int priority;
+  char *formatp; 
+
+  ckarg(2);
+  priority=ckintval(argv[0]);
+  formatp=argv[1]->c.str.chars;
+  syslog(priority, formatp);
+  fprintf(stderr, "SYSLOG: %d %s\n", priority, formatp);
+  return(NIL); }
+
+
+pointer euslog(context *ctx, int n, pointer argv[])
+{ pointer mod=argv[0];
+
+  defun(ctx,"OPENLOG",mod,OPENLOG);
+  defun(ctx,"SYSLOG", mod, SYSLOG);
+  return(T);
+  }
+
